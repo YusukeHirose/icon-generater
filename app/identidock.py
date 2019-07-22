5808c717ec87d827491407c38bb0be0aaadb88e7
kdoebr21 <kodeberic@yahoo.co.jp>
2019-07-23 00:44:48 +0900

    feat: htmlを返却

--- app/identidock.py
+++ app/identidock.py
@@ -1,9 +1,24 @@
 from flask import Flask
 app = Flask(__name__)
+default_name = 'yusuke'
 
 @app.route("/")
-def hello_world():
-    return "Hello World!\n"
+def get_icon():
+    name = default_name
+
+    header = '<html><head><title>Icon</title></head><body>'
+
+    body = '''<form method="POST">
+              Hello <input type="text" name="name" value="{}">
+              <input type="submit" value="submit">
+              </form>
+              <p> You look like a:
+              <img src="/a/a.png"/>
+              '''.format(name)
+              
+    footer = '</body></html>'
+
+    return header + body + footer
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
