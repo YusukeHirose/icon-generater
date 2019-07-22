d8bbb85f2c7f90a01de644c4dc9b9588883aaec9
kdoebr21 <kodeberic@yahoo.co.jp>
2019-07-23 01:17:05 +0900

    feat: dnmonsterのapiを叩いて画像表示

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:latest
 
 RUN groupadd -r uwsgi && useradd -r -g uwsgi uwsgi
-RUN pip install --upgrade pip Flask uWSGI
+RUN pip install --upgrade pip Flask uWSGI requests
 
 WORKDIR /app
 
--- app/identidock.py
+++ app/identidock.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, Response
+import requests
+
 app = Flask(__name__)
 default_name = 'yusuke'
+dnmonster_api_path = 'http://dnmonster:8080/monster/'
 
 @app.route("/")
-def get_icon():
+def get_template():
     name = default_name
 
     header = '<html><head><title>Icon</title></head><body>'
@@ -13,12 +16,19 @@ def get_icon():
               <input type="submit" value="submit">
               </form>
               <p> You look like a:
-              <img src="/a/a.png"/>
+              <img src="/user/a.png"/>
               '''.format(name)
-              
+
     footer = '</body></html>'
 
     return header + body + footer
+
+@app.route("/user/<name>")
+def get_icon(name):
+    response = requests.get(dnmonster_api_path + name + "?size=80")
+    image = response.content
+
+    return Response(image, mimetype='image/png')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
